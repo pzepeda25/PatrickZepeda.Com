77280d46a8dfcaddb17e25b83c959c6076b0d1d5
Patrick Zepeda <me@patrickzepeda.com>
2026-03-30 06:57:18 -0500

    feat: Add featured project images
    Includes image assets for the featured projects section.

--- src/components/FeaturedProject.tsx
+++ src/components/FeaturedProject.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { ExternalLink, Sparkles, Code } from 'lucide-react';
 import { motion } from 'motion/react';
 
-import imageForgeImg from '../assets/xml-image-forge/image_forge.png';
-import dachshundChefImg from '../assets/xml-image-forge/dachshund-chef.png';
-import tvFishBowlImg from '../assets/xml-image-forge/tv-fish-bowl.png';
-import lakeCabinImg from '../assets/xml-image-forge/lake-cabin.png';
+import imageForgeImg from '../assets/image_forge.png';
+import dachshundChefImg from '../assets/dachshund-chef.png';
+import tvFishBowlImg from '../assets/tv-fish-bowl.png';
+import lakeCabinImg from '../assets/lake-cabin.png';
 
 const FeaturedProject = () => {
   return (
